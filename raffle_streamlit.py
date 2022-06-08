04cd7a927fd862b5d747984031ff02eb1715cbf7
Tim Tennyson <ttennyson.xyz@gmail.com>
2022-06-07 21:20:27 -0700

    added requirements.txt for web3 (issues) and updated the streamlit app to match the raffle engine

--- raffle_streamlit.py
+++ raffle_streamlit.py
@@ -6,11 +6,35 @@ import pandas as pd
 import hashlib
 
 #Added the following imports-- TT
-import web3
+#import web3
+
 from web3 import Web3
 
+#Connect to web3 server
+# Old Ganache Server w3=Web3(web3.HTTPProvider('http://127.0.0.1:7545'))
+#New Ganache Server:
+w3=Web3(web3.HTTPProvider('http://127.0.0.1:8545'))
 
-#Price per ticket in Pwei
+#load accounts
+w3.eth.accounts
+
+#Local contract address from remix when deployed:
+contract_address='0x7912bd9E742a81c620FF0FcF1A9Dc6b9De543f69'
+
+#import json and open contract_abi.txt (local abi, found in remix under "compile" tab when deployed)
+import json
+with open('contract_abi.txt') as f: 
+    contract_abi=json.load(f)
+
+#set the raffle_contract variable to the local contract address and the contract abi to call the contract
+raffle_contract=w3.eth.contract(address=contract_address, abi=contract_abi)
+
+#call the house function to get the house address
+raffle_contract.functions.house().call()
+
+
+    
+#Price per ticket in ETH
 price_per_ticket = 1
 current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 #drawing_date = current_date + timedelta(weeks=1)
@@ -25,14 +49,17 @@ st.markdown("## Purchase a ticket using ethereum and win big!")
 st.sidebar.text(current_date)
 
 #Sidebar to display number of Entries
-st.sidebar.text(f"Next Drawing: {drawing_date}")
+#Original st.sidebar.text(f"Next Drawing: {drawing_date}")
+st.sidebar.text("Drawing will be held when there are 5 entries!")
+st.sidebar.text(f"Number of Entries: {raffle_contract.functions.getLength().call()}")
 
-#st.sidebar.text(f"Next Drawing: {drawing_date}")
+#Set the value to 1 ether
+value_wei=w3.toWei(1, 'ether')
 
 st.sidebar.markdown("## Tickets are available at 1 ETH each")
 
 #Creating a streamlit data input  for sender
-address = st.sidebar.text_input("Ethereum Address")
+address_sender = st.sidebar.text_input("Ethereum Address")
 
 #Creating a streamlit data input  for Receiver
 Tickets = st.sidebar.number_input("Tickets",min_value=1,step=1)
@@ -46,27 +73,34 @@ if 'purchased' not in st.session_state:
 def callback(): 
     st.session_state.purchased=True
 
+
 #Displaying the purchase button and asking for confirmation
 purchased = st.sidebar.button("Purchase", on_click=callback)
 if purchased or st.session_state.purchased:
     st.write("Please confirm the following information:")
-    st.write(f"Ethereum Address: {address}")
+    st.write(f"Ethereum Address: {address_sender}")
     st.write(f"Number of Tickets: {Tickets}")
     st.write(f"Your total cost is: {Cost} ETH")
+    
+
 
 #Confirmation button
     if st.button("Confirm"):
-        st.write(f" Congratulations {address}! You have purchased {Tickets} tickets for {Cost} ETH.")
+        st.write(f" Congratulations {address_sender}! You have purchased {Tickets} tickets for {Cost} ETH.")
         st.balloons()
+        
+        #send the transaction from the input address
+        transaction={'from': address_sender, 'value': value_wei*Cost}
+        raffle_contract.functions.enter().transact(transaction)
 
-        #Increasing the pot
-        pot += Cost
-        st.session_state.purchased=False
+        #Increasing the pot-- no longer needed since we call the getLength function to display number of entries
+        #pot += Cost
+        #st.session_state.purchased=False
 
     #Cancel button
     elif st.button("Cancel"):
         st.write("Order Cancelled")
         st.session_state.purchased=False
 
-#Displaying the current pot
-st.sidebar.text(f"The current pot is {pot} ETH")
+#Displaying the current pot-- already done through the contract
+#st.sidebar.text(f"The current pot is {pot} ETH")
